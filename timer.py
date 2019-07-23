a9f0086a23076b25be307181af06e9f6557f9683
ali.raza <ali.raza@automotive-ai.com>
2019-07-23 11:46:13 +0200

    feat(timeit): add timeit decorator

---
+++ timer.py
@@ -0,0 +1,38 @@
+import time
+
+
+def timeit(precision=2, iters=1):
+    if callable(precision):
+        def wrapper(*args, **kw):
+            ts = time.time()
+            result = precision(*args, **kw)
+            te = time.time()
+
+            print("Elapsed time({}): {:.2f} ms".format(precision.__name__, (te - ts) * 1000))
+
+            return result
+
+        return wrapper
+    else:
+        def decorator(func):
+            def wrapper(*args, **kw):
+                if iters > 1:
+                    ts = time.time()
+                    for i in range(iters):
+                        result = func(*args, **kw)
+                    te = time.time()
+
+                    taken = (te - ts) * 1000.0 / iters
+                else:
+                    ts = time.time()
+                    result = func(*args, **kw)
+                    te = time.time()
+                    taken = (te - ts) * 1000.0
+
+                print("Elapsed time({1}): {2:.{0}f} ms".format(precision, func.__name__, taken))
+
+                return result
+
+            return wrapper
+
+        return decorator
